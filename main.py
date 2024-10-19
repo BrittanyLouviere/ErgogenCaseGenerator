0c7282d219fb9f20ea7239f7167fcfd129705451
Brittany <67611757+BrittanyLouviere@users.noreply.github.com>
2024-10-18 21:26:47 -0500

    Read Yaml Export to stl and svg

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import os
+
 import cadquery as cq
+import yaml
+
 
 # run `cq-editor` in terminal and open this file in it for debugging
 
 def read_ergogen_file(file_path: str):
-    pass
+    with open(file_path) as stream:
+        return yaml.safe_load(stream)
 
-def generate_case():
+
+# units in mm
+def generate_case(ergogen_config):
     return cq.Workplane("front").box(2.0, 2.0, 0.5)
 
-if __name__ == '__main__':
-    pass
 
-read_ergogen_file("example.yaml")
-result = generate_case()
+ergogen_config = read_ergogen_file("output/example.yaml")
+result = generate_case(ergogen_config)
+
+if __name__ == '__main__':
+    os.makedirs("output", exist_ok=True)
+    result.export("output/case.stl")
+    result.export("output/case.svg")
