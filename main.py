9b938c094a8c2465d6e5729c425b2c91747193da
Brittany <67611757+BrittanyLouviere@users.noreply.github.com>
2024-10-20 12:49:27 -0500

    Expand compound keys in config

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ import yaml
 
 # run `cq-editor main.py` in terminal and open this file in it for debugging
 
-def read_ergogen_file(file_path: str):
+def read_ergogen_file(file_path: str) -> dict:
     with open(file_path) as stream:
-        return yaml.safe_load(stream)
+        config = yaml.safe_load(stream)
+    config = expand_keys(config)
+    return config
 
+
+def expand_keys(dictionary: dict) -> dict:
+    newDictionary = {}
+    for key, value in dictionary.items():
+        val = expand_keys(value) if type(value) is dict else value
+        if '.' in key:
+            key1, key2 = key.split('.', 1)
+            newDictionary[key1] = {key2: val}
+        else:
+            newDictionary[key] = val
+    return newDictionary
 
 # units in mm
 def generate_case(ergogen_config):
